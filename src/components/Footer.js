7d7fa36e3bf284073f71e515fc5f07c27eb9ac4a
Patricia Grammetbauer <info@PatriciaGrammetbauer.de>
2022-01-05 15:38:23 +0100

    Router 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,29 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 
 const Footer = () => {
     const info =[
-        {name:'Max Mustermann',
-        mail:'MaxMusterman@gmail.com'},
+        {mail:'MaxMusterman@gmail.com'},
 
         {author:'Max Mustermann',
         mail:'MaxMusterman@gmail.com'},
     ]
 
     return(
-        <div className='Footer'>
-           <NavLink to='/contact' className='Contact'>contact</NavLink>
+        <div>
+             <div className='Footer'>
+            <NavLink to='/contact' className='Contact'>contact</NavLink>
             <NavLink to='/imprint' className='Imprint'>imprint</NavLink>
             <a className='Source' href="https://de.freepik.com/vektoren/buch">image source</a>
             
-     <Routes>
+    
+        </div>
+        <div className='Routes'>
+        <Routes>
       <Route path='/contact' element={<Info data={info[0]}/>} />
       <Route path='/imprint' element={<Info data={info[1]}/>} />
     </Routes>
         </div>
+        </div>
+       
     )
 }
 
