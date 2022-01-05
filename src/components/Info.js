cc4d9001ecc5054456a5fa88ecba55810044963c
Patricia Grammetbauer <info@PatriciaGrammetbauer.de>
2022-01-05 15:33:25 +0100

    Router

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,17 @@
 
+import './Info.scss'
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 function Info({data}){
 
-    const{name, mail, author} = data
+    const{mail, author} = data
 
     return(
-        <div>
-            <h3>{name}</h3>
-            <h3>{author}</h3>
-            <h3>{mail}</h3>
+        <div className='container'>
             
+            <h3>{author} </h3>
+            <h3>{mail} </h3>
+            <NavLink className='back' style={{ color: 'white' , textDecoration: 'none', marginTop: '.5em' }}to='/' className='Home'>X</NavLink>
         </div>
     )
 }
